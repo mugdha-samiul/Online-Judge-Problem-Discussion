6387db64fa7424dcbb819d5abb590573c4a70fde
Samiul Islam Mugdha <67468157+mugdha-samiul@users.noreply.github.com>
2022-09-15 02:48:10 +0600

    Create 10104 - Euclid Problem.cpp

---
+++ UVa/10104 - Euclid Problem.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll x, y, d;
+ll gcdExtended(ll a, ll b, ll& x, ll& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    d = gcdExtended(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    ll a, b;
+    while(cin >> a >> b){
+        //if(a>b)swap(a,b);
+        d = gcdExtended(a, b, x, y);
+        cout << x << " " << y << " " << d << endl;
+    }
+    return 0;
+}
